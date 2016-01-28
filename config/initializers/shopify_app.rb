8730497ddae545da72a9e692621f7728d5685448
Michel Giguere <michel.giguere@shopify.com>
2016-01-28 14:05:51 -0500

    generate shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.api_key = "a6b71f97627e670b6ff51e8676ee46c1"
+  config.secret = "a339ca9c94b4158cabfb0b9e80f8e43c"
+  config.redirect_uri = "<redirect_uri>"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
