6e384e1a51e471372d63ed38f3a28dc3735465ab
Michel Giguere <michel.giguere@shopify.com>
2016-01-28 14:21:33 -0500

    change to uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "a6b71f97627e670b6ff51e8676ee46c1"
   config.secret = "a339ca9c94b4158cabfb0b9e80f8e43c"
-  config.redirect_uri = "http://fast-ridge-43073.herokuapp.com/auth/shopify/callback"
+  config.redirect_uri = "fast-ridge-43073.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
