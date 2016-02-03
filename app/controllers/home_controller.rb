b929c6ccd3206531613422a670a81559166c9475
Michel Giguere <michel.giguere@shopify.com>
2016-02-03 12:45:13 -0500

    remove attempt at pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,4 @@ class HomeController < AuthenticatedController
   def index
     @events = ShopifyAPI::Event.find(:all, :params => {:page => 1})
   end
-
-  def page2
-  	@events = ShopifyAPI::Event.find(:all, :params => {:page => 2})
-  end
 end
