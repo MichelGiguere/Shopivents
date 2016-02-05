f5b1adab38ba0da0aedb6fe24a1fb937124d9e13
Michel Giguere <michel.giguere@shopify.com>
2016-02-05 13:14:33 -0500

    fix spacing in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-	def change_url (event.message)
+	def change_url(event.message)
 		message1.gsub(/[=]/, '=' '=http://#{@shop_session.url}')
 		message2.gsub(/["]/, '')
 	end
