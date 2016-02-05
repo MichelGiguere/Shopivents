5cd8b8c5e0ed71da1ce8755e62639481c59d9b5b
Michel Giguere <michel.giguere@shopify.com>
2016-02-05 13:23:33 -0500

    not https

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def change_url(url)
-		a = url.gsub(/[=]/, '=' 'https://#{@shop_session.url}')
+		a = url.gsub(/[=]/, '=' 'http://#{@shop_session.url}')
 		b = a.gsub(/["]/, '')
 		return b
 	end
