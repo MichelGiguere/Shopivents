c410daee5ce37908644f26dbd974b5491cc77320
Michel Giguere <michel.giguere@shopify.com>
2016-02-08 15:51:14 -0500

    forgot the return

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
 		a = url.gsub(/[href=]/, "href=https://#{@shop_session.url}")
 		b = url.gsub(/<a/, "<a target='_blank'")
 		c = a.gsub(/["]/, '')
-		return b
+		return c
 	end
 end
