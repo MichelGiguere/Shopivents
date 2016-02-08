ab7024217cc186cec815dc496727244a281a1402
Michel Giguere <michel.giguere@shopify.com>
2016-02-08 14:14:58 -0500

    back to blank

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def change_url(url)
 		a = url.gsub(/[=]/, "=https://#{@shop_session.url}")
-		b = url.gsub(/<a/, "<a target='_self'")
+		b = url.gsub(/<a/, "<a target='_blank'")
 		c = a.gsub(/["]/, '')
 		return b
 	end
