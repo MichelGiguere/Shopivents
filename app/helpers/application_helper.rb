a21a4befa74b94f692d7f754d3efcd50bd9417d3
Michel Giguere <michel.giguere@shopify.com>
2016-02-08 13:40:36 -0500

    keep trying

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def change_url(url)
-		a = url.gsub(/[=]/, "'=https://#{@shop_session.url} target='_blank'")
+		a = url.gsub(/[=]/, "=https://#{@shop_session.url} target='_blank'")
 		b = a.gsub(/["]/, '')
 		return b
 	end
