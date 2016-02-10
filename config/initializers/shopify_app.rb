b8459c1ab98b6cf2f0b4c375b311b25962b95bb3
Michel Giguere <michel.giguere@shopify.com>
2016-02-10 13:12:58 -0500

    redo scope

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.api_key = "a6b71f97627e670b6ff51e8676ee46c1"
   config.secret = "a339ca9c94b4158cabfb0b9e80f8e43c"
   config.redirect_uri = "https://fast-ridge-43073.herokuapp.com/auth/shopify/callback"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products, read_content, read_shipping, read_fulfillments, read_customers"
   config.embedded_app = true
 end
