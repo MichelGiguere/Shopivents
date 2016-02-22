207d1f373e5523425e2504af7c45a2206cb2177e
Michel Giguere <michel.giguere@shopify.com>
2016-02-22 14:11:16 -0500

    different test regex

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 	def change_url(url)
 		a = url.gsub(/[<a href=[^]+?>]/, '')
-		b = a.gsub(/[<\/a>]/, '')
-		return b
+		return a
 	end
+	
 	def fix_time(time)
 		x = time.gsub(/[T]/, ", ")
 		y = x.gsub(/-05:00/, '')
