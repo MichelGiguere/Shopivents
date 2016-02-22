e16c50fada50464efdff9556adc0268eda9eab06
Michel Giguere <michel.giguere@shopify.com>
2016-02-22 14:17:45 -0500

    go back to original

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
 	def change_url(url)
-		a = url.gsub(/[<a href=[^]+?>]/, '')
+		a = url.gsub(/[=]/, "=https://#{@shop_session.url}")
+		b = a.gsub(/["]/, '')
+		return b
 	end
-
 	def fix_time(time)
 		x = time.gsub(/[T]/, ", ")
 		y = x.gsub(/-05:00/, '')
