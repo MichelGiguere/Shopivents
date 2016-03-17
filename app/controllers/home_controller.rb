2868bfe44fc2f6e297c37c81aadc6eebbae5b641
Michel Giguere <michel.giguere@shopify.com>
2016-03-17 16:00:24 -0400

    remove unused API call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,4 @@ class HomeController < AuthenticatedController
   def index
     @events = ShopifyAPI::Event.find(:all, :params => {:page => 1})
   end
-
-  def page2
-  	@events2 = ShopifyAPI::Event.find(:all, :params => {:page => 2})
-  end
 end
