ad037653b8fded9048f59370be24d451c914af0b
Michel Giguere <michel.giguere@shopify.com>
2016-04-27 11:33:23 -0400

    troubleshoot api calls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < AuthenticatedController
   def index
     @events = ShopifyAPI::Event.find(:all, :params => {:page => 1})
-
-    @orders = ShopifyAPI::Event.find(:all, :params => {:order, :page =>1})
-    @products = ShopifyAPI::Event.find(:all, :params => {:product, :page =>1})
+    
   end
 end
