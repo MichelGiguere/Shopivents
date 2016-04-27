d5daca00468f55e3830e651de4a562506937378f
Michel Giguere <michel.giguere@shopify.com>
2016-04-27 11:29:15 -0400

    add new api calls for tabs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < AuthenticatedController
   def index
     @events = ShopifyAPI::Event.find(:all, :params => {:page => 1})
+
+    @orders = ShopifyAPI::Event.find(:all, :params => {:order, :page =>1})
+    @products = ShopifyAPI::Event.find(:all, :params => {:product, :page =>1})
   end
 end
