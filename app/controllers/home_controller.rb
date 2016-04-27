3f7aa413aafebf0cc4f1c246abe3dab36cb301f1
Michel Giguere <michel.giguere@shopify.com>
2016-04-27 12:37:11 -0400

    api call test for products and orders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < AuthenticatedController
   def index
     @events = ShopifyAPI::Event.find(:all, :params => {:page => 1})
-    
+
+    @orders = ShopifyAPI::Event.find(:all, :params => {:filter => 'Order'})
+    @products = ShopifyAPI::Event.find(:all, :params => {:filter => 'Product'})
+
   end
 end
