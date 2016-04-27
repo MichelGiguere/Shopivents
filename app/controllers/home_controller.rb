9b28a088b89ef17778bbac0efc4d9cffa2a2122a
Michel Giguere <michel.giguere@shopify.com>
2016-04-27 13:34:53 -0400

    limit orders and products api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < AuthenticatedController
   def index
     @events = ShopifyAPI::Event.find(:all, :params => {:page => 1})
 
-    @orders = ShopifyAPI::Event.find(:all, :params => {:filter => 'Order'})
-    @products = ShopifyAPI::Event.find(:all, :params => {:filter => 'Product'})
+    @orders = ShopifyAPI::Event.find(:all, :params => {:filter => 'Order', :limit => 50})
+    @products = ShopifyAPI::Event.find(:all, :params => {:filter => 'Product', :limit =>50})
 
   end
 end
